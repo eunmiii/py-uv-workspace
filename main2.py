89cb227d15224e22f2aed23794ddad7ee2497a2c
eunmiii <em000516@kookmin.ac.kr>
2026-07-14 17:21:52 +0900

    commit 260714

---
+++ main2.py
@@ -0,0 +1,6 @@
+
+from my_shapes.area import circle_area, square_area
+
+print("커스텀 패키지 실행 결과")
+print(f"반지름 5인 원의 넓이 : {circle_area(5)}")
+print(f"가로 4, 세로 5 사각형 넓이 {square_area(4,5)}")
---
+++ my_shapes/area.py
@@ -0,0 +1,6 @@
+
+def circle_area(r) :
+    return 3.15 * r * r
+
+def square_area(w,h) :
+    return w*h
